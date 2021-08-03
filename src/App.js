332a48a3e926e08236ba00f432632cac4f0289a2
Oscar Powell <oscar.t.powell@gmail.com>
2021-08-03 20:39:12 +0100

    Adding identity widget 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useIdentityContext,IdentityContextProvider } from "react-netlify-identity-widget";
+import "react-netlify-identity-widget/styles.css"
+
+const IdentityModal = React.lazy(() => import("react-netlify-identity-widget"))
 
 function App() {
+  const url = "https://react-notification-app.netlify.app/";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IdentityContextProvider value={url}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </IdentityContextProvider>
   );
 }
+
+function Main() {
+  const identity = useIdentityContext()
+  const [dialog, setDialog] = React.useState(false)
+  const isLoggedIn = identity && identity.isLoggedIn
+  return (
+    <div className="App">
+      <button className="btn" onClick={() => setDialog(isLoggedIn)}>
+        {isLoggedIn ? "LOG OUT" : "LOG IN"}
+      </button>
+      <React.Suspense fallback="loading...">
+        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
+      </React.Suspense>
+    </div>
+  )
+}
+
+
 
 export default App;
