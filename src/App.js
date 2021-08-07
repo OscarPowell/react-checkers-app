9a79a1cb00a182c686d7413184b692543ee4484c
Oscar Powell <oscar.t.powell@gmail.com>
2021-08-07 14:31:59 +0100

    Created spinning Aang

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import meme_aang_cutout from './meme_aang_cutout.png';
 import './App.css';
-import { useIdentityContext,IdentityContextProvider } from "react-netlify-identity-widget";
-import "react-netlify-identity-widget/styles.css"
-
-const IdentityModal = React.lazy(() => import("react-netlify-identity-widget"))
+import FirstComponent from './FirstComponent';
 
 function App() {
-  const url = "https://react-notification-app.netlify.app/";
-  return (
-    <IdentityContextProvider value={url}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </IdentityContextProvider>
-  );
-}
-
-function Main() {
-  const identity = useIdentityContext()
-  const [dialog, setDialog] = React.useState(false)
-  const isLoggedIn = identity && identity.isLoggedIn
   return (
     <div className="App">
-      <button className="btn" onClick={() => setDialog(isLoggedIn)}>
-        {isLoggedIn ? "LOG OUT" : "LOG IN"}
-      </button>
-      <React.Suspense fallback="loading...">
-        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
-      </React.Suspense>
+      <header className="App-header">
+        <img src={meme_aang_cutout} className="App-logo" alt="logo" />
+        <p>
+          Hello Mira
+        </p>
+        
+      </header>
+      {/* <FirstComponent displaytext="First Component Data"/> */}
     </div>
-  )
+  );
 }
-
-
 
 export default App;
