3f59ac21cd04d6210a139c173167af7718922a0b
Oscar Powell <oscar.t.powell@gmail.com>
2021-08-30 11:48:43 +0100

    Add Menu

---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Menu.css';
+
+export default class Menu extends React.Component {
+    render() {
+        return  (
+            <div class="menu-inner"></div>
+        );
+    }
+}
+
